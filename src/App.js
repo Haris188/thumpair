be77a3e36ff74b5f93a1f8ce5feaef6b50cce1b0
= <=>
2024-12-19 10:19:48 -0500

    analytics

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   ThemeProvider,
   createMuiTheme
 } from '@material-ui/core'
+import { useLocation } from 'react-router-dom';
 
 const themeData = {
   palette:{
@@ -18,6 +19,37 @@ const themeData = {
   }
 }
 
+
+const Analytics = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const registerVisit = async () => {
+      try {
+        const response = await fetch('https://lenz-api.bigcapy.com/increment', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            app_id: process.env.REACT_APP_LENZ_KEY,
+            path: location.pathname,
+          }),
+        });
+        const result = await response.text();
+        console.log(result);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    registerVisit();
+  }, [location.pathname]);
+
+  return null;
+};
+
+
 function App() {
   const routing = useRoutes(routes)
   const theme = createMuiTheme(themeData)
@@ -25,6 +57,7 @@ function App() {
   return (
     <ThemeProvider theme = {theme}>
       <div className="App" style = {{height:'100%'}}>
+        <Analytics />
         {routing}
       </div>
     </ThemeProvider>
